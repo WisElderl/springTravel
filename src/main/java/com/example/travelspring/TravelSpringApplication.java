d7ff194db5004e24ef0e380e44337728aa95a8a3
Tara Routray <dev@tararoutray.com>
2022-07-09 20:10:57 +0600

    Start

---
+++ src/main/java/com/example/travelspring/TravelSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travelspring/TravelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
